fe604f57f409744fa5bd349c363f6966da27727b
Diego Vilhalva <diegorocha450@yahoo.com>
2025-02-19 13:32:51 -0300

    hero & hero filter created

--- app/(web)/page.tsx
+++ app/(web)/page.tsx
@@ -1,9 +1,9 @@
-import Image from "next/image";
+import HeroSection from "./_components/hero-section";
 
 export default function Home() {
   return (
    <div className="w-full">
-    
+    <HeroSection/>
    </div>
   );
 }
