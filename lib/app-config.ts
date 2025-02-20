38d94f217388469f96e03d811f2f3ec6349084a6
Diego Vilhalva <diegorocha450@yahoo.com>
2025-02-20 12:10:13 -0300

    appwrite setup

---
+++ lib/app-config.ts
@@ -0,0 +1,11 @@
+
+
+export const APP_CONFIG = {
+    APPWRITE:{
+        KEY:process.env.NEXT_APPWRITE_KEY!,
+        ENDPOINT:process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT!,
+        PROJECT_ID:process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID!,
+        DATABASE_ID:process.env.NEXT_PUBLIC_APPWRITE_DATABASE!,
+        SHOP_ID:process.env.NEXT_PUBLIC_APPWRITE_COLLECTION_ID!
+    }
+}
